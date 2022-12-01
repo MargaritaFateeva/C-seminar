0e3fbafa11a37f0fe5a4fb734d36a418c824bb8b
Margarita <margarita.gubina@mail.ru>
2022-12-02 00:41:59 +0700

    enter num1&num2 finish yes/no num1==num2*num2

--- task1/Program.cs
+++ task1/Program.cs
@@ -11,7 +11,7 @@ int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 
-if (number2 == number1*number1)
+if (number1 == number2*number2)
 {
 Console.WriteLine("да");
 }
