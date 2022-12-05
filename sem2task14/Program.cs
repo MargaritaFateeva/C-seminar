179de3874c9909c88fcbaa3626e5f05e74168367
Margarita <margarita.gubina@mail.ru>
2022-12-06 01:36:04 +0700

    enter number finish yes/no reminder of division 7 and 23

---
+++ sem2task14/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №14. Работа в группах Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23. 
+// ●14 -> нет ●46 -> нет ●161 -> да
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int ost7 = N%7;
+int ost23 = N%23;
+
+if (ost7==0)
+{
+    if (ost23==0)
+    {
+        Console.WriteLine($"{N} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{N} -> нет");
+    }
+}
+else
+{
+    Console.WriteLine($"{N} -> нет");
+}
