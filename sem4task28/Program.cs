d973fe68bcc6e0d6be4554da1bc62750a4ef3f0b
Margarita <margarita.gubina@mail.ru>
2022-12-09 15:22:24 +0700

    method enter number a finish composition 1 to a

---
+++ sem4task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"Произведение чисел = {GetMulti(num)}");
+
+int GetMulti (int number)
+{
+    int comp = 1;
+    while (number>0)
+    {
+        Console.Write($" {number} ");
+        comp = comp*number;
+        number--;
+    }
+    return comp;
+}
