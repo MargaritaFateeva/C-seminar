860578d325c795d876bac4a0e2a693ed678b4909
Margarita <margarita.gubina@mail.ru>
2022-12-10 02:19:56 +0700

    enter number a and b finish z-degree calculator num a in b-degree

---
+++ dz4zadacha27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("ВВедите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"{N} -> {SumNumeric(N)}");
+
+int SumNumeric (int number)
+{
+    int sum=0;
+    while (number>0)
+    {
+        sum=sum+number%10;
+        number=number/10;
+    }
+    return sum;
+}
