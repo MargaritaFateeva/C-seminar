7d7dc3d8d8361841ba9c3859ed94a45ea788853c
Margarita <margarita.gubina@mail.ru>
2022-12-09 13:15:59 +0700

    enter coordinats 3  points A and B finish find distance between points A and B

---
+++ dz3zadacha21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+Console.Write("Точка A введите координату X: ");
+int Xa = int.Parse(Console.ReadLine());
+Console.Write("Точка А введите координату Y: ");
+int Ya = int.Parse(Console.ReadLine());
+Console.Write("Точка А введите координату Z: ");
+int Za = int.Parse(Console.ReadLine());
+
+Console.Write("Точка B введите координату X: ");
+int Xb = int.Parse(Console.ReadLine());
+Console.Write("Точка B введите координату Y: ");
+int Yb = int.Parse(Console.ReadLine());
+Console.Write("Точка А введите координату Z: ");
+int Zb = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt ((Xb-Xa)*(Xb-Xa) + (Yb-Ya)*(Yb-Ya) + (Zb-Za)*(Zb-Za));
+Console.Write($"Расстояние между двумя точками A и B: {d}");
