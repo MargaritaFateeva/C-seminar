838a5155c94ccd4808ef7f220c695417a65d90aa
Margarita <margarita.gubina@mail.ru>
2022-12-09 15:03:19 +0700

    method enter number  finish count num in number

---
+++ sem4task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт 
+// количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число N {GetCountNum (N)}-значное");
+
+int GetCountNum (int a)
+{
+    int count = 0;
+    while (a>0)
+    {
+        a=a/10;
+        count++;
+    }
+return count;
+}
