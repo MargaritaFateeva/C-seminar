3dfd7c15af20f54ffdeedf83dc70082fcc5b656b
Margarita <margarita.gubina@mail.ru>
2022-12-20 19:04:04 +0700

    change negative elements of array to positive and conversily

---
+++ sem5task32/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GetArray(int size)
+{
+    int[]res = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        res[i]=new Random().Next(-10,11);
+    }
+    return res;
+}
+
+void PrintArray(int[]arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}    
+
+void GetChangeElement(int[] arr)
+{
+    Console.Write($"-> ");
+    for(int i=0; i<arr.Length; i++)
+    {
+        arr[i]=arr[i]*(-1);
+        Console.Write($" {arr[i]} ");
+    }
+}
+
+int [] array = GetArray(4);
+PrintArray(array);
+Console.WriteLine();
+
+GetChangeElement(array);
