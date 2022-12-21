18cea5f867a186bafafb0de72510bd190e9a4be5
Margarita <margarita.gubina@mail.ru>
2022-12-21 23:50:25 +0700

    check sum of 2 sides of the triangle less then 3td side

---
+++ sem6task40/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40:
+// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите 3 числа, после каждого нажать ВВОД");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+ 
+ void Check(int a, int b, int c)
+ {
+    bool yes=true;
+    if(a>b+c || b>a+c || c>b+a) yes=false;
+    // if(a>b+c) yes=false;
+    // if(b>a+c) yes=false;
+    // if(c>b+a) yes=false;
+    if (yes==true) Console.WriteLine("Треугольник возможен");
+    if (yes==false) Console.WriteLine("Треугольник невозможен");
+ }
+
+ Check(a,b,c);
