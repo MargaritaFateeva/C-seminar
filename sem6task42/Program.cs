0629db2192240d143e82ee5bc23f5099b77e3a12
Margarita <margarita.gubina@mail.ru>
2022-12-22 02:10:49 +0700

    convert enter number in 10-system to 2-system

---
+++ sem6task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42:
+// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45--> 101101
+// 3--> 11
+// 2--> 10
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int begin = n;
+int res=n;
+string numDouble = "";
+while (n>0)
+{
+    res=n%2;
+    numDouble = res+numDouble;
+    n=n/2;
+}
+Console.WriteLine($"Число {begin} в двоичной системе -> {numDouble}");
