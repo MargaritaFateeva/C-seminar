b592480a364ac26b53162c1f24417fe5e620dbfb
Margarita <margarita.gubina@mail.ru>
2022-12-25 16:28:21 +0700

    zadacha 63

---
+++ sem9task63/Program.cs
@@ -0,0 +1,28 @@
+﻿//ВАРИАНТ ВЫВОДА ЧЕРЕЗ СТРОЧНЫЙ МАССИВ
+string LineNumbers(int n)
+{
+return (n>=2) ? LineNumbers(n-1) + $" {n}" : "1";
+
+// if(n>=2) 
+// {
+//    return LineNumbers(n-1) + $" {n}";
+// }
+// else
+// {
+// return "1";
+// }
+
+}
+Console.WriteLine(LineNumbers(10));
+
+
+
+
+//ВАРИАНТ ВЫВОДА VOID
+void LineNumbers2(int n)
+{
+    if(n != 1) LineNumbers2(n - 1);
+    Console.Write($"{n} ");
+}
+LineNumbers2(5);
+
