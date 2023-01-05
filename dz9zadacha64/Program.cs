aa796e68563fa4d71bc90c52753c206b60ce8ada
Margarita <margarita.gubina@mail.ru>
2023-01-06 03:56:15 +0700

    recursion - show numbers from n to 1

---
+++ dz9zadacha64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string GetNumberDescending (int a, int b) //1 способ
+{
+    if (b<=a) return GetNumberDescending(a,b+1)+ $"{b} ";
+    else return String.Empty;
+}
+
+void GetNumDescending (int a, int b) //2 способ
+{
+    if(b<a) GetNumDescending(a, b+1);
+    Console.Write($"{b} ");
+}
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(GetNumberDescending(n,1));
+
+Console.WriteLine();
+GetNumDescending(n,1);
